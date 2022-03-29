ca2d7540e062019db5d86371f47bd7e351a2036b
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-29 17:59:05 +0900

    chore: show a button

--- gitdocker/gitdocker.py
+++ gitdocker/gitdocker.py
@@ -6,6 +6,11 @@ class GitDocker(DockWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Git docker")
+        self.widget = QWidget()
+        self.main_layout = QHBoxLayout()
+        self.main_layout.addWidget(QPushButton(i18n('Foo')))
+        self.widget.setLayout(self.main_layout)
+        self.setWidget(self.widget)
 
     def canvasChanged(self, canvas):
         pass
