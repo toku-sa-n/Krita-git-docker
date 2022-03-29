47431338a0793427262fb357b0decfccab09ae63
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-29 14:01:52 +0900

    chore: add `setup.py`

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+DESC = 'A Krita plugin for viewing Krita files in other git revisions.'
+
+setup(
+    name='gitdocker',
+    version='0.1',
+    description=DESC,
+    author='Hiroki Tokunaga',
+    author_email='tokusan441@gmail.com',
+    url='https://github.com/toku-sa-n/Krita-git-docker',
+    packages=['gitdocker'],)
