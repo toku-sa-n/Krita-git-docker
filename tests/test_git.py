42f37f9d6540555eec1d49b0c84627b5f485cdba
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-29 14:51:00 +0900

    test: add for `git init`

--- tests/test_git.py
+++ tests/test_git.py
@@ -1,9 +1,16 @@
 import unittest
+import tempfile
+from git import Repo
 
 
 class TestGitMethods(unittest.TestCase):
     def test_success(self):
         self.assertEqual(1, 1)
+
+    def test_git_init(self):
+        dir = tempfile.TemporaryFile()
+        repo = Repo.init(dir.name)
+        assert not repo.bare
 
 
 if __name__ == '__main__':
