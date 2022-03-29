30dff5624ce735d62e18b4ee82c29baae0c87800
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-29 14:51:36 +0900

    test: remove an useless test

--- tests/test_git.py
+++ tests/test_git.py
@@ -4,9 +4,6 @@ from git import Repo
 
 
 class TestGitMethods(unittest.TestCase):
-    def test_success(self):
-        self.assertEqual(1, 1)
-
     def test_git_init(self):
         dir = tempfile.TemporaryFile()
         repo = Repo.init(dir.name)
