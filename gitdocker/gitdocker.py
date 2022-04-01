4d687ed77d3e5e233c5f5310351edca22969ed29
Hiroki Tokunaga <tokusan441@gmail.com>
2022-04-01 16:43:41 +0900

    feat: show the thumbnail

--- gitdocker/gitdocker.py
+++ gitdocker/gitdocker.py
@@ -1,7 +1,12 @@
 from krita import *
 from PyQt5.QtWidgets import *
+from io import BytesIO
+import subprocess
 from typing import Optional
 from git import Repo
+import zipfile
+import os
+from pathlib import Path
 import itertools
 
 
@@ -54,8 +59,43 @@ class GitDocker(DockWidget):
         else:
             return None
 
+    def get_thumbnail(self, HEXSHA):
+        RAW = self.get_revision(HEXSHA)
+
+        if RAW is None:
+            return None
+
+        thumbnail = None
+        EXTENSION = Path(self.path).suffix
+        if EXTENSION == '.kra':
+            UNCOMPRESSED = zipfile.ZipFile(BytesIO(RAW), "r")
+            thumbnail = QImage.fromData(UNCOMPRESSED.read("mergedimage.png"))
+            if thumbnail is None:
+                thumbnail = QImage.fromData(UNCOMPRESSED.read("preview.png"))
+
+        if thumbnail is None:
+            self.label.setText("No thumbnail available")
+            return None
+
+        self.label.setPixmap(QPixmap.fromImage(thumbnail))
+
+    def get_revision(self, HEXSHA):
+        if self.path is None:
+            return None
+
+        if self.repo is None:
+            return None
+
+        RELPATH = os.path.relpath(self.path, self.repo.working_tree_dir)
+
+        p = subprocess.Popen(["git", "show", "%s:%s" %
+                             (HEXSHA, RELPATH)], stdout=subprocess.PIPE, cwd=self.repo.working_tree_dir)
+        out, _ = p.communicate()
+        print(out)
+        return out
+
     def commit_combo_box_current_index_changed(self, index):
-        self.label.setText(self.commits[index].hexsha)
+        self.get_thumbnail(self.commits[index].hexsha)
 
 
 Krita.instance().addDockWidgetFactory(DockWidgetFactory(
